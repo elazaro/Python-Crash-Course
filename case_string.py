b050b9471fb6b433c04bd346769b7258e269cbff
elazaro <elazaro@mxrisc.com>
2022-01-06 20:33:37 -0600

    NEW: case conversion script

---
+++ case_string.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.9
+
+# some question should come with this very simple code, first: who's Ada Love-
+# lace, ;) google it and show respect.
+# second: is Python OO, or only partially supporting objects, because the title
+# function looks pretty much like a method.
+# third: is this implicit variable a String object? what other methods this ob-
+# ject supports.
+# fourth: is the title behaviour customizable?
+
+person = "ada lovelace";
+print(person.title());
+
+# these other methods are the typical upper case and lower case converters.
+
+person = "ADA LOVELACE";
+print(person.lower());
+
+person = "ada lovelace";
+print(person.upper());
+
+# differently to other languajes, this methods, upper and lower are not named
+# toUpper or toLower, is this a convention?
+
