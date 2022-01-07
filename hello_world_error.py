8bbf1e411a3feacabe5dcecbe25ef8db1978c34f
elazaro <elazaro@mxrisc.com>
2022-01-06 19:17:22 -0600

    CHANGE: Added shebang line to python scripts.

--- hello_world_error.py
+++ hello_world_error.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3.9
 #It is interesting how Python, seems to be loosely typed, but; is it?
-message = "Hello Python world!"
+mesage = "Hello Python world!"
 #the print function is nice, it doesn't require too much effort, additionaly, no semicolon? never?
 print(message)
