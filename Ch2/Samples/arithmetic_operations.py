cb1e9561b77a29f76dc0a5f74f1ff4157607b7ed
elazaro <elazaro@mxrisc.com>
2022-01-11 22:54:25 -0600

    Arithmetic operations sample

---
+++ Ch2/Samples/arithmetic_operations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.9
+
+value = 3 + 1
+print(value);
+value = 4 - 2
+print(value);
+value = 8 / 2
+print(value);
+value = 8 ** 2
+print(value)
+value = 2 + 8**2
+print(value)
+value = (2 + 8)**2
+print(value)
+
